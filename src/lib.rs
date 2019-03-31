43d4df14eee49804d8b93a4c75d586d2f8ccf3d9
Alessandro Genova <ales.genova@gmail.com>
2019-03-30 20:11:15 -0400

    Export the newly created modules

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,13 @@
 pub mod float;
 pub mod vector;
 pub mod ray;
+pub mod hit;
 pub mod hitable;
 pub mod camera;
 pub mod scene;
 pub mod renderer;
+pub mod material;
+pub mod actor;
 
 #[cfg(test)]
 mod tests {
