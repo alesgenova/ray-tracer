1f9964218b65fc84074eb9ab4e5c8c3265b1e114
Alessandro Genova <ales.genova@gmail.com>
2019-03-31 12:09:52 -0400

    The normal in the Hit struct is already normalized

--- src/material/metal.rs
+++ src/material/metal.rs
@@ -28,8 +28,7 @@ impl<T> Material<T> for MetalMaterial<T>
 {
     fn scatter(&self, incident: &Ray<T>, hit: &Hit<T>) -> Scatter<T> {
         let attenuation = Vec3::<T>::from_slice(self.color.get_data());
-        let mut normal = Vec3::from_slice(hit.normal.get_data());
-        normal.normalize();
+        let normal = &hit.normal;
         let origin = Vec3::from_slice(hit.point.get_data());
         let mut direction = MetalMaterial::<T>::reflect(incident.get_direction(), &normal);
         direction.normalize();
